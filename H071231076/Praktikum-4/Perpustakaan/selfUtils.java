59c3a727e43063adb6fab916c1a06b01eb3013cd
Adrian29-gpu <putradrian29@gmail.com>
2024-04-21 11:03:03 +0800

    TP4_H071231076

--- H071231076/Praktikum-4/Perpustakaan/selfUtils.java
+++
@@ -1,8 +0,0 @@
-package Perpustakaan;
-
-public class selfUtils {
-    public static void displayselfdata(){
-        System.out.println("Nama: Adrian Tri Putra");
-        System.out.println("NIM: H071231076");
-    }
-}
