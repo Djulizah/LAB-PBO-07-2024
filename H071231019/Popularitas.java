dac7dabb5d5f88d5bd77a4f048dbd250362f581d
Suci Zaitun Nurkamal <143978837+SuciZaitunNurkamal@users.noreply.github.com>
2024-04-22 17:51:03 +0800

    Tugas Praktikum 3

---
+++ H071231019/Popularitas.java
@@ -0,0 +1,11 @@
+class Popularitas{
+    double popularity;
+
+    Popularitas(double popularity) {
+        this.popularity = popularity;
+    }
+
+    void display() {
+        System.out.println("Popularity: " + this.popularity);
+    }
+}
